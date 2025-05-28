9883a2cc4e54fbe1bb9b09884349e853b2890f43
RJH_DESK\rujun <rujung1225@naver.com>
2025-05-28 21:11:54 +0900

    2025-05-28 (Linked List) 문제풀이

---
+++ algo/src/linked_list/Remove_Nth_Node_From_End_of_List.java
@@ -0,0 +1,48 @@
+package linked_list;
+
+public class Remove_Nth_Node_From_End_of_List {
+    public static void main(String[] args) {
+        ListNode root = new ListNode(1);
+        root.next = new ListNode(2);
+        removeNthFromEnd(root, 2);
+    }
+
+    public static ListNode removeNthFromEnd(ListNode head, int n) {
+       int cnt = 0;
+       ListNode cur = head;
+       while(cur != null) {
+           cur = cur.next;
+           cnt++;
+       }
+       cur = head;
+       if(cnt - n == 0) {
+           head = head.next;
+           return head;
+       }
+       ListNode prev = head;
+       while(cnt - n > 0) {
+           prev = cur;
+           cur = cur.next;
+           cnt--;
+       }
+       prev.next = cur.next;
+       return head;
+    }
+
+    static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+}
