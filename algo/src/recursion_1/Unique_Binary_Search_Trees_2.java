283ec11a638a5aa9c9791f473694b21555db2e10
RJH_DESK\rujun <rujung1225@naver.com>
2025-07-27 17:42:40 +0900

    2025-07-27 (recur1) 문제풀이

--- algo/src/recursion_1/Unique_Binary_Search_Trees_2.java
+++ algo/src/recursion_1/Unique_Binary_Search_Trees_2.java
@@ -7,17 +7,24 @@ import java.util.List;
 
 public class Unique_Binary_Search_Trees_2 {
     public static void main(String[] args) {
+        generateTrees(3);
     }
 
-    public List<TreeNode> generateTrees(int n) {
+    public static List<TreeNode> generateTrees(int n) {
         if (n == 0) return new ArrayList<>();
         return build(1, n);
     }
 
     /**
-     * 솔직히 못풀었다 이거 재귀 반복 머리속에서 뱅글뱅글 돌다가 이해가 안됨 .복습 필요
+     * 루트를 처음반복 즉 1 ~ n 까지 모두 루트가 되는데 예를들어 n 이 3이라면
+     * 1부터 루트가 되어서 왼쪽 에서 만들 수 있는 모든 경우와 오른쪽에서 만들 수 있는 모든 경우를 합쳐서 붙이게 된다.
+     * 베이스 케이스는 시작 이 끝보다 작은 경우  null 이 되어야 한다.
+     *
+     * 1이 루트인 경우 먼저 보게 되면 1에서(1,0) 은 왼쪽 트리는 없기 때문에 제외하고
+     * (2, 3) -> [(2, 2), (3, 3)] -> (2, 2) -> [(2, 1), (3, 2)] 이런식으로 왼쪽 트리와 오른쪽 트리를 만들어서
+     * 붙이게 되고 그게 최종 결과에 쓰이게 된다. 
      */
-    private List<TreeNode> build(int start, int end) {
+    private static List<TreeNode> build(int start, int end) {
         List<TreeNode> trees = new ArrayList<>();
 
         if (start > end) {
